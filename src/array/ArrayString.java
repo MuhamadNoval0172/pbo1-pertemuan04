815c2d523b1711405d71a281a2087a771a8cf606
muhamadnoval <mnoval292@gmail.com>
2020-04-25 09:41:29 +0700

    Array String

---
+++ src/array/ArrayString.java
@@ -0,0 +1,17 @@
+package array;
+
+public class ArrayString {
+    public static void main(String[] args) {
+        String matakuliah[];
+        matakuliah = new String[10];
+
+        matakuliah[0] = "Pemrograman Berbasis Objek 1";
+        matakuliah[1] = "Visual 2";
+        matakuliah[3] = "Rekayasa Perangkat Lunak";
+        matakuliah[4] = "Organisasi Komputer";
+        matakuliah[5] = "Pemrograman web 2";
+        matakuliah[6] = "Jaringan Komputer";
+
+        System.out.println(matakuliah[5]);
+    }
+}
